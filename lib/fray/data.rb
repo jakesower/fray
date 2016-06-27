f077217e9b16e68a164f8df3df236141d887fa47
Jake Sower <jsower@esri.com>
2016-06-27 16:48:20 -0400

    fixing closure issues for Fray Data

--- lib/fray/data.rb
+++ lib/fray/data.rb
@@ -6,43 +6,35 @@
 module Fray::Data
   class Base < OpenStruct
     def initialize(hash)
-      if VALIDATOR.(hash)
+      if JSON::Validator.validate(__schema, hash)
         super(hash)
         self.freeze
       else
         e = "#{hash.inspect} failed schema validation:\n\n" +
-            ERROR_GENERATOR.(hash).join("\n")
+            JSON::Validator.fully_validate(__schema, hash).join("\n")
         raise ArgumentError, e
       end
     end
   end
 
-
   #
   # Do the dirty work of compiling the data JSON.
   #
-  dir = File.dirname(File.expand_path(__FILE__))
-  Dir["#{dir}/data_structures/*.json"].each do |path|
-    file_name = File.basename(path, '.json')
-    schema = JSON.parse(File.read(path))
-
-    validator = JSON::Validator.method(:validate).curry[schema]
-    error_generator = JSON::Validator.method(:fully_validate).curry[schema]
-
+  def self.define_schema_class(name, schema)
+    const_name = name.split('_').map{|chunk| chunk[0].upcase + chunk[1..-1]}.join('')
     klass = Class.new(Base) do
-      VALIDATOR = validator
-      ERROR_GENERATOR = error_generator
-
-      def initialize(hash)
-        super(hash)
+      define_method :__schema do
+        schema
       end
     end
 
-    const_name = file_name.
-      split('_').
-      map{|chunk| chunk[0].upcase + chunk[1..-1]}.
-      join('')
-
-    Fray::Data.const_set(const_name.to_sym, klass)
+    Fray::Data.const_set(const_name.to_sym, klass.freeze)
   end
+end
+
+# prime the class
+dir = File.dirname(File.expand_path(__FILE__))
+Dir["#{dir}/data_structures/*.json"].each do |path|
+  file_name = File.basename(path, '.json')
+  Fray::Data.define_schema_class(file_name, JSON.parse(File.read(path)))
 end
